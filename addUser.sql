f18a6410852dbe43e6fff9915bd37b7f90985d59
MatStep <matstepanek2@gmail.com>
2015-08-29 00:38:41 +0200

    DB added user SQL file

---
+++ addUser.sql
@@ -0,0 +1,3 @@
+INSERT INTO `user` (`id`, `name`, `password`, `email`, `reg_date`, `role`) VALUES
+(1,	'admin',	'$2y$10$LT4tHNVCez2lDkNmS7SUTOYLrn3ySbokG6b5/LjyFakKXKZKqbq5y',	'',	'2015-08-28 23:37:13',	NULL);
+
