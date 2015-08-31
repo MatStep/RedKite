a0c4cd12d268dba36eced96d9861d51d6a225359
MatStep <matstepanek2@gmail.com>
2015-08-31 16:00:14 +0200

    Edited right to Admin, categories added

--- www/app/AdminModule/presenters/BasePresenter.php
+++ www/app/AdminModule/presenters/BasePresenter.php
@@ -12,5 +12,10 @@ class BasePresenter extends \App\Presenters\BasePresenter
 	public function startup()
     {
     	parent::startup();
+    	$user = $this->getUser();
+    	if(!$user->isLoggedIn()) {
+    		$this->flashMessage('Pre vstup do administrácie je potrebné sa prihlásiť');
+    		$this->redirect(':Homepage:');
+    	}
     }
 }
---
+++ www/app/AdminModule/presenters/CategoryPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Nette;
+use App\Model;
+
+/**
+ * Category presenter
+ */
+class CategoryPresenter extends \App\AdminModule\Presenters\BasePresenter
+{
+
+	public function renderDefault()
+	{
+
+	}
+
+}
