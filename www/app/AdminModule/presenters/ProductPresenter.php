8aad0e572e23f83e5d26ced8e8f335fcce898e61
MatStep <matstepanek2@gmail.com>
2015-09-08 12:16:29 +0200

    Product default added, Manager added

--- www/app/AdminModule/presenters/ProductPresenter.php
+++ www/app/AdminModule/presenters/ProductPresenter.php
@@ -3,7 +3,8 @@
 namespace App\AdminModule\Presenters;
 
 use Nette,
-	App\Model;
+	App\Model,
+	Nette\Application\UI\Form as Form;
 
 /**
  * Product presenter
@@ -13,8 +14,91 @@ class ProductPresenter extends \App\AdminModule\Presenters\BasePresenter
 	/** @var \App\Model\UserManager @inject */
 	public $userManager;
 
+	private $products;
+
+	private $values;
+
+	private $id;
+	
+	public function __construct(Model\ProductManager $products)
+	{
+		$this->products = $products;
+		$this->values = array("name" => "", "short_desc" => "", "desc" => "", "status" => "", "order" => "", "price_sell" => "");
+		$this->id = 0;
+	}
+
 	public function renderDefault()
 	{
+		$this->template->products = $this->products->getAll();
+	}
+
+	/*Product form*/
+	public function createComponentProductForm()
+	{
+		$form = new Form;
+
+		$form->addText("name", "Názov")
+			 ->setRequired('Názov je povinný')
+			 ->getControlPrototype()->class("form-control");
+
+		$form->addText("value", "Hodnota")
+			 ->setRequired('Hodnota je povinná')
+			 ->getControlPrototype()->class("form-control");
+
+		$form->addSubmit("add", "Pridať daň")
+			 ->getControlPrototype()->class("btn btn-primary pull-right");
+
+		$form->addSubmit("edit", "Uložiť zmeny")
+			 ->getControlPrototype()->class("btn btn-primary pull-right");
+
+		$form->onSuccess[] = array($this, "productFormSucceeded");
+
+		return $form;
+	}
+
+	public function productFormSucceeded($form, $values)
+	{
+		$adding = true;
+		
+		try {
+			if ( isset($this->request->getParameters()['productId']) )
+			{
+				$productId = $this->getParameter('productId');
+				$adding = false;
+			}
+
+			if ($adding)
+			{
+				$this->products->insert($values);
+				$this->flashMessage('Produkt úspešne pridaný');
+			}
+			else
+			{
+				$this->products->edit($productId, $values);
+				$this->flashMessage('Produkt bol aktualizovaný');
+			}
+
+				$this->redirect("Product:");
+		} catch (Nette\Application\BadRequestException $e) {
+			if ($e->getMessage() == "NAME_EXISTS")
+				$form->addError('Produkt neexistuje');
+		}
+	}
+
+	public function actionRemove($productId)
+	{
+			$this->products->remove($productId);
+			$this->flashMessage('Daň bola úspešne vymazaná');
+			$this->redirect("Product:");
+	}
+
+	public function actionEdit($productId)
+	{
+		$product = $this->products->getProduct($productId);
+
+		$this->template->productId = $productId;
+
+		$this['productForm']->setDefaults($product->toArray());
 
 	}
 }
---
+++ www/app/model/ProductManager.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Model;
+
+use Nette,
+	App\Model,
+	Exception;
+
+/**
+ * Product management.
+ */
+class ProductManager extends Nette\Object
+{
+	const 
+		PRODUCT_TABLE = 'product',
+		COLUMN_ID = 'id',
+		COLUMN_NAME = 'name',
+		COLUMN_SHORT_DESC = 'short_desc',
+		COLUMN_DESC = 'desc',
+		COLUMN_STATUS = 'status',
+		COLUMN_ORDER = 'order',
+		COLUMN_ADD_DATE = 'add_date',
+		COLUMN_PRICE_SELL = 'price_sell',
+
+		BRAND_TABLE = 'brand',
+		COLUMN_BRAND_ID = 'brand_id';
+
+	/** @var Nette\Database\Context */
+	private $database;
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database   = $database;
+	}
+
+	public function getAll()
+	{
+		return $this->database->table('product');
+	}
+
+	/*Get product*/
+	public function getProduct($productId)
+	{
+		$product =  $this->database->table(self::PRODUCT_TABLE)->where(self::COLUMN_ID, $productId)->fetch();
+
+		if ( !$product )
+		{
+			throw new Nette\Application\BadRequestException("DOESNT_EXIST");
+		}
+
+		return $product;
+	}
+
+	public function insert($values)
+	{
+		if ($this->database->table(self::PRODUCT_TABLE)->where(self::COLUMN_NAME, $values->name)->count() > 0)
+			throw new Nette\Application\BadRequestException("NAME_EXISTS");
+
+		$data = array();
+		$data["name"]  = $values->name;
+		$data["short_desc"] = $values->short_desc;
+		$data["desc"] = $values->desc;
+		$data["status"] = $values->status;
+		$data["price_sell"] = $values->price_sell;
+		$data["brand_id"] = $values->brand_id;
+
+		return $this->database->table(self::PRODUCT_TABLE)->insert($data);
+
+	}
+
+	public function edit($id, $values)
+	{
+		$product = $this->database->table(self::PRODUCT_TABLE)->where(self::COLUMN_ID, $id);
+
+		if (!$product)
+		{
+			throw new Nette\Application\BadRequestException("DOESNT_EXIST");
+		}
+
+		$product->update(array(
+			self::COLUMN_NAME => $values->name,
+			self::COLUMN_SHORT_DESC => $values->short_desc,
+			self::COLUMN_DESC => $values->desc,
+			self::COLUMN_STATUS => $values->status,
+			self::COLUMN_PRICE_SELL => $values->price_sell,
+			self::COLUMN_BRAND_ID => $values->brand_id,
+			));
+	}
+
+	public function remove($id, $state = "parent")
+	{
+		return $this->database->table(self::PRODUCT_TABLE)->where(self::COLUMN_ID, $id)->delete();
+	}
+}
