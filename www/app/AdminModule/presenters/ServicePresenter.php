52f010bbe0fff2d0e467036ae4f661abf83c9661
xjahic <jahic.jakub@gmail.com>
2015-09-11 00:13:45 +0200

    added product edit, added basic Services - add doesnt work
    vyhadzuje error pri pridanie dalsej sluzby.. nevedel som vyriesit zatial

---
+++ www/app/AdminModule/presenters/ServicePresenter.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\AdminModule\Presenters;
+
+use Nette,
+	App\Model,
+	Nette\Application\UI\Form as Form;
+
+/**
+ * Service presenter
+ */
+class ServicePresenter extends \App\AdminModule\Presenters\BasePresenter
+{
+	private $services;
+
+	private $values;
+
+	private $id;
+
+
+	public function __construct(Model\ServiceManager $services)
+	{
+		$this->services = $services;
+		$this->values = array("name" => "", "desc" => "");
+		$this->id = 0;
+
+	}
+
+	public function renderDefault()
+	{
+		$this->template->services = $this->services->getAll()->order('name');
+	}
+
+    /*Service form*/
+	protected function createComponentServiceForm()
+    {
+        $form = new Form;
+
+        $form->addText("name", "Názov služby")
+             ->setRequired('Názov je povinný')
+             ->getControlPrototype()->class("form-control");
+
+        $form->addText("desc", "Popis")
+             ->setRequired('Popis je povinný')
+             ->getControlPrototype()->class("form-control");
+
+        $form->addSubmit("add", "Prida službu")
+             ->getControlPrototype()->class("btn btn-primary pull-right");
+
+        $form->addSubmit("edit", "Uloži zmeny")
+             ->getControlPrototype()->class("btn btn-primary pull-right");
+
+        $form->onSuccess[] = array($this, "serviceFormSucceeded");
+
+        return $form;
+    }
+
+    public function ServiceFormSucceeded($form, $values)
+    {
+    	$adding = true;
+
+    	if ( isset($this->request->getParameters()['serviceId']) )
+    	{
+    		$serviceId = $this->getParameter('serviceId');
+    		$adding = false;
+    	}
+
+    	try {
+    		if ($adding)
+            {
+                $this->services->insert($values);
+                $this->flashMessage('Služba úspešne pridaná');
+            }
+            else
+            {
+                $this->services->edit($serviceId, $values);
+                $this->flashMessage('Sluzba bola aktualizovaná');
+            }
+
+    		$this->redirect('Service:');
+
+    	} catch (Nette\Application\BadRequestException $e) {
+    		if ($e->getMessage() == "NAME_EXISTS")
+    			$form->addError('Názov služby už existuje');
+    	}
+    }
+
+	public function actionRemove($serviceId)
+	{
+		$this->services->remove($serviceId);
+
+		$this->flashMessage('Služba bola úspešne vymazaná');
+		$this->redirect("Service:");
+	}
+
+	public function actionEdit($serviceId)
+	{
+		$service = $this->services->getService($serviceId);
+
+		$this->template->serviceId = $serviceId;
+
+		$this['serviceForm']->setDefaults($service->toArray());
+
+	}
+
+}
---
+++ www/app/model/ServiceManager.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Model;
+
+use Nette,
+	App\Model,
+	Exception;
+
+/**
+ * service management.
+ */
+class ServiceManager extends Nette\Object
+{
+	const 
+		SERVICE_TABLE = 'service',
+		COLUMN_ID = 'id',
+		COLUMN_NAME = 'name',
+		COLUMN_DESC = 'desc';
+
+
+	/** @var Nette\Database\Context */
+	private $database;
+
+	public function __construct(Nette\Database\Context $database)
+	{
+		$this->database   = $database;
+	}
+
+	public function getAll()
+	{
+		return $this->database->table('service');
+	}
+
+	/*Get service*/
+	public function getservice($serviceId)
+	{
+		$service =  $this->database->table(self::SERVICE_TABLE)->where(self::COLUMN_ID, $serviceId)->fetch();
+
+		if ( !$service )
+		{
+			throw new Nette\Application\BadRequestException("DOESNT_EXIST");
+		}
+
+		return $service;
+	}
+
+	public function insert($values)
+	{
+		if ($this->database->table(self::SERVICE_TABLE)->where(self::COLUMN_NAME, $values->name)->count() > 0)
+			throw new Nette\Application\BadRequestException("NAME_EXISTS");
+
+		$data = array();
+		$data["name"]  = $values->name;
+		$data["desc"] = $values->desc;
+
+		return $this->database->table(self::SERVICE_TABLE)->insert($data);
+
+	}
+
+	public function edit($id, $values)
+	{
+		$service = $this->database->table(self::SERVICE_TABLE)->where(self::COLUMN_ID, $id);
+
+		if (!$service)
+		{
+			throw new Nette\Application\BadRequestException("DOESNT_EXIST");
+		}
+
+		$service->update(array(
+			self::COLUMN_NAME => $values->name,
+			self::COLUMN_DESC => $values->desc,
+			));
+	}
+
+	public function remove($id, $state = "parent")
+	{
+		return $this->database->table(self::SERVICE_TABLE)->where(self::COLUMN_ID, $id)->delete();
+	}
+}
